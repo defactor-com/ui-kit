6b84a3917351a8380070ad1bd84ead09eaa679ae
AngeloCG97 <angelocas13_8@hotmail.com>
2024-01-18 15:23:51 -0600

    feat(components): added fontFamily param

--- src/components/MenuOption/index.tsx
+++ src/components/MenuOption/index.tsx
@@ -5,16 +5,18 @@ export interface IMenuOption extends React.HTMLAttributes<HTMLButtonElement> {
   text?: string;
   icon?: string;
   color?: string;
+  fontFamily?: string;
   isSelected: Boolean;
 }
 
 export const MenuOption: React.FC<IMenuOption> = React.memo(
-  ({ icon, text, color, isSelected, ...props }: IMenuOption) => (
+  ({ icon, text, color, isSelected, fontFamily, ...props }: IMenuOption) => (
     <button
       className={clsx("menu-option", { selected: isSelected })}
       style={{
         borderRight: isSelected ? `2px solid ${color}` : "none",
         color: window.innerWidth < 912 && isSelected ? color : "",
+        fontFamily: fontFamily,
       }}
       {...props}
     >
--- src/components/Pill/index.tsx
+++ src/components/Pill/index.tsx
@@ -5,16 +5,23 @@ export interface IPill {
   label: string;
   color: string;
   bgColor: string;
+  fontFamily?: string;
   externalStyles: string;
 }
 
-export const Pill = ({ label, color, bgColor, externalStyles }: IPill) => {
+export const Pill = ({
+  label,
+  color,
+  bgColor,
+  externalStyles,
+  fontFamily,
+}: IPill) => {
   return (
     <div
       className={clsx(externalStyles, "container-pill")}
       style={{ backgroundColor: bgColor }}
     >
-      <span style={{ color: color }}>{label}</span>
+      <span style={{ color: color, fontFamily: fontFamily }}>{label}</span>
     </div>
   );
 };
