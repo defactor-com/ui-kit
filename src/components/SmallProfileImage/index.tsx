156f800bba8844b925fd9972a07a81d098d37386
AngeloCG97 <angelocas13_8@hotmail.com>
2024-01-19 10:47:25 -0600

    feat(components): improved code styles

--- src/components/SmallProfileImage/index.tsx
+++ src/components/SmallProfileImage/index.tsx
@@ -7,11 +7,11 @@ export interface ISmallProfileImage {
   imgChild?: React.ReactElement;
 }
 
-export const SmallProfileImage: React.FC<ISmallProfileImage> = ({
+export const SmallProfileImage = ({
   image,
   imgChild,
   bgColor = "#F8F9FC",
-}) => (
+}: ISmallProfileImage) => (
   <div className="avatar-container">
     <div className="avatar-container" style={{ backgroundColor: bgColor }}>
       {image && <img src={image} className="app-avatar" alt="avatar" />}
