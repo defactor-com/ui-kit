a24f2a4d877e7f33d07807fffc13f4f5f35dc2b0
JAMares <josuegerardojgam885@gmail.com>
2024-03-20 14:17:54 -0600

    fix(lendBorrow): unused code deleted

--- src/components/Input/index.tsx
+++ src/components/Input/index.tsx
@@ -1,4 +1,4 @@
-import React, { ChangeEvent, useState, useEffect } from "react";
+import React, { ChangeEvent, useState } from "react";
 
 export type InputValue = string | number | readonly string[] | undefined;
 
