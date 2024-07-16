e3f69f6c0eaae1bd8c3f481459b85822b4abff3d
pzagardedukic <petra.zagar.dedukic@gmail.com>
2024-07-16 09:44:13 +0200

    feat(CardWithHover): Added Hover and Icon Copy. #316

--- src/components/CardWithHover/index.tsx
+++ src/components/CardWithHover/index.tsx
@@ -1,6 +1,7 @@
 import React from 'react';
-import { Box, Typography, useTheme } from '@mui/material';
+import { Box, Typography, useTheme, IconButton, Button } from '@mui/material';
 import DocIcon from '../Icons/v2/docIcon';
+import CopyTemplateIcon from '../Icons/v2/copyTemplateIcon';
 
 export interface CardWithHoverProps {
     title: string;
@@ -8,6 +9,8 @@ export interface CardWithHoverProps {
     onSelect?: () => void;
     icon: React.ReactNode;
     backgroundColor?: string;
+    onClickPreview?: () => void;
+    onClickUse?: () => void;
 }
 
 export const CardWithHover: React.FC<CardWithHoverProps> = ({
@@ -15,9 +18,20 @@ export const CardWithHover: React.FC<CardWithHoverProps> = ({
     description,
     onSelect,
     icon = <DocIcon />,
-    backgroundColor = '#ffffff'
+    backgroundColor = '#ffffff',
+    onClickPreview,
+    onClickUse
 }) => {
     const theme = useTheme();
+    const [anchorEl, setAnchorEl] = React.useState<HTMLButtonElement | null>(null);
+
+    const handleClick = (event: React.MouseEvent<HTMLButtonElement>) => {
+        setAnchorEl(event.currentTarget);
+    };
+
+    const handleClose = () => {
+        setAnchorEl(null);
+    };
 
     return (
         <Box
@@ -27,13 +41,29 @@ export const CardWithHover: React.FC<CardWithHoverProps> = ({
                 padding: 2,
                 cursor: 'pointer',
                 maxWidth: '400px',
+                '&:hover .hover-it': {
+                    display: 'flex'
+                },
                 '&:hover': { boxShadow: theme.shadows[3] }
             }}
             onClick={onSelect}
         >
-            <Box display="flex" flexDirection="column" justifyContent="space-between" height={154}>
+            <Box display="flex" flexDirection="column" justifyContent="space-between" height={170}>
                 <Box display="flex" justifyContent="space-between" alignItems="center" mb={2}>
                     {icon}
+                    <IconButton
+                        className='hover-it'
+                        sx={{
+                            display: anchorEl ? 'block' : 'none',
+                            p: 0,
+                            width: '32px',
+                            height: '32px',
+                            borderRadius: '50%'
+                        }}
+                        onClick={handleClick}
+                    >
+                        <CopyTemplateIcon />
+                    </IconButton>
                 </Box>
                 <Box>
                     <Typography variant="body1" fontWeight={500}>
@@ -42,6 +72,36 @@ export const CardWithHover: React.FC<CardWithHoverProps> = ({
                     <Typography variant="caption" sx={{ color: '#7c7c7e' }}>
                         {description}
                     </Typography>
+                </Box>
+                <Box
+                    className='hover-it'
+                    sx={{
+                        display: 'none',
+                        width: '100%',
+                        mt: 2,
+                        gap: 2
+                    }}
+                >
+                    <Box sx={{ width: '100%' }}>
+                        <Button
+                            fullWidth
+                            variant='outlined'
+                            sx={{ borderRadius: '100px' }}
+                            onClick={onClickPreview}
+                        >
+                            Preview
+                        </Button>
+                    </Box>
+                    <Box sx={{ width: '100%' }}>
+                        <Button
+                            fullWidth
+                            variant='contained'
+                            sx={{ borderRadius: '100px', whiteSpace: 'nowrap' }}
+                            onClick={onClickUse}
+                        >
+                            Use Template
+                        </Button>
+                    </Box>
                 </Box>
             </Box>
         </Box>
