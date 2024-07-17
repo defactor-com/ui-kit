80d6b9fede4a2d8085ca365a41ad63e686fd5796
pzagardedukic <petra.zagar.dedukic@gmail.com>
2024-07-17 09:40:04 +0200

    feat(Popover): Added Actions. #317

--- src/stories/Popover.stories.tsx
+++ src/stories/Popover.stories.tsx
@@ -1,10 +1,10 @@
-import React from "react";
+import React, { useState } from "react";
 import type { Meta, StoryObj } from "@storybook/react";
 import Popover, { PopoverProps, ListItemProps } from "../components/Popover";
 import EditIcon from "@mui/icons-material/Edit";
 import DeleteIcon from "@mui/icons-material/Delete";
 import ContentCopyIcon from "@mui/icons-material/ContentCopy";
-import { action } from '@storybook/addon-actions';
+import { action } from "@storybook/addon-actions";
 
 const meta: Meta<typeof Popover> = {
     title: "V2/Popover",
@@ -24,10 +24,30 @@ const popoverItems: ListItemProps[] = [
     { text: "Delete", icon: <DeleteIcon /> },
 ];
 
+const PopoverTemplate: React.FC<PopoverProps> = (args) => {
+    const [anchorEl, setAnchorEl] = useState<HTMLButtonElement | null>(null);
+
+    const handleOpen = (event: React.MouseEvent<HTMLButtonElement>) => {
+        setAnchorEl(event.currentTarget);
+    };
+
+    const handleClose = () => {
+        setAnchorEl(null);
+        args.onClose();
+    };
+
+    return (
+        <>
+            <button onClick={handleOpen}>Open Popover</button>
+            <Popover {...args} anchorEl={anchorEl} onClose={handleClose} />
+        </>
+    );
+};
+
 export const Default: Story = {
+    render: (args) => <PopoverTemplate {...args} />,
     args: {
         items: popoverItems,
-        anchorEl: document.createElement("button"),
-        onClose: action('closed'),
+        onClose: action("closed"),
     },
 };
