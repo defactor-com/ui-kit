3fc169df8b23320e01e65f6ea1527d1b399d4a4d
pzagardedukic <petra.zagar.dedukic@gmail.com>
2024-07-23 13:27:21 +0200

    fix(StatsBox): Added Font Family. #355

--- src/components/ResponsiveToggle/StatsBox/index.tsx
+++ src/components/ResponsiveToggle/StatsBox/index.tsx
@@ -1,15 +1,15 @@
-import { Box, Divider, Typography } from '@mui/material'
-import React from 'react'
+import { Box, Divider, Typography } from "@mui/material";
+import React from "react";
 
-import { CustomTooltip } from '../../CustomTooltip'
+import { CustomTooltip } from "../../CustomTooltip";
 
 export interface StatsBoxProps {
-    XsWidth?: string | number
-    MdWidth?: string | number
-    title: string
-    value: string | number
-    tooltipText: string
-    unit: string | null
+    XsWidth?: string | number;
+    MdWidth?: string | number;
+    title: string;
+    value: string | number;
+    tooltipText: string;
+    unit: string | null;
 }
 
 export const StatsBox: React.FC<StatsBoxProps> = ({
@@ -18,44 +18,46 @@ export const StatsBox: React.FC<StatsBoxProps> = ({
     title,
     value,
     tooltipText,
-    unit
+    unit,
 }) => {
     return (
         <Box
             sx={{
                 width: {
                     xs: XsWidth,
-                    md: MdWidth
-                }
+                    md: MdWidth,
+                },
             }}
         >
             <Box
                 sx={{
-                    display: 'flex',
-                    flexDirection: 'row',
-                    alignItems: 'center',
-                    justifyContent: 'space-between'
+                    display: "flex",
+                    flexDirection: "row",
+                    alignItems: "center",
+                    justifyContent: "space-between",
                 }}
             >
-                <Typography variant='body2'>{title}</Typography>
+                <Typography variant="body2" fontFamily="'DM Sans', sans-serif">
+                    {title}
+                </Typography>
                 <CustomTooltip tooltipText={tooltipText} />
             </Box>
             <Box
                 sx={{
-                    display: 'flex',
-                    flexDirection: 'row',
-                    alignItems: 'baseline',
-                    mt: 0.6
+                    display: "flex",
+                    flexDirection: "row",
+                    alignItems: "baseline",
+                    mt: 0.6,
                 }}
             >
-                <Typography variant='subtitle1' fontWeight={700} sx={{ mr: 0.5 }}>
+                <Typography variant="subtitle1" fontFamily="'DM Sans', sans-serif" fontWeight={700} sx={{ mr: 0.5 }}>
                     {value}
                 </Typography>
-                <Typography fontSize={14} fontWeight={700} color='#A8B0B6'>
+                <Typography fontSize={14} fontWeight={700} color="#A8B0B6" fontFamily="'DM Sans', sans-serif">
                     {unit}
                 </Typography>
             </Box>
             <Divider sx={{ mt: 2.5 }} />
         </Box>
-    )
-}
+    );
+};
