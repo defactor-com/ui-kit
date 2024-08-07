cce9b2a7fe4330283a8ce035db23c435d89c4626
AngeloCG97 <angelocas13_8@hotmail.com>
2024-08-07 17:04:39 -0600

    feat(sidebar): updated styles

--- src/components/Sidebar/SidebarTypes.ts
+++ src/components/Sidebar/SidebarTypes.ts
@@ -1,7 +1,14 @@
+import { StaticImport } from "next/dist/shared/lib/get-img-props";
 import React from "react";
 
+export type AppDataType = {
+  logo: string | StaticImport;
+  url?: string;
+};
 export interface ISidebar {
+  mainApp: AppDataType;
   externalStyles?: string;
+  appsData: Array<AppDataType>;
   menuOptions?: React.ReactNode;
   optionalStyles?: React.CSSProperties;
 }
