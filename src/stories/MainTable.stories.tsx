775805e886fa77a5b472a1fc9828ff300cb9b460
pzagardedukic <petra.zagar.dedukic@gmail.com>
2024-10-07 14:52:21 +0200

    feat(v3): MainTable Basic Storybook File. #409

---
+++ src/stories/MainTable.stories.tsx
@@ -0,0 +1,25 @@
+import React from "react";
+import { Story, Meta } from "@storybook/react";
+import { MainTable, MainTableProps } from "../components/V3/MainTable"
+
+export default {
+    title: "V2/MainTable",
+    component: MainTable,
+    argTypes: {
+        // color: { control: "color" },
+    },
+} as Meta;
+
+const Template: Story<MainTableProps> = (args) => (
+    <MainTable {...args} />
+);
+
+export const ActiveTitleWithIcon = Template.bind({});
+ActiveTitleWithIcon.args = {
+    label: "1"
+};
+
+export const InactiveTitleWithIcon = Template.bind({});
+InactiveTitleWithIcon.args = {
+    label: "2"
+};
