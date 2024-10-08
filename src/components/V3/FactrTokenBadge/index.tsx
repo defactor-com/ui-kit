6733e0508094574375662ad63bf17fb0eb250242
pzagardedukic <petra.zagar.dedukic@gmail.com>
2024-10-08 12:39:56 +0200

    feat(v3): Fixing FactrTokenBadge v3. #409

--- src/components/V3/FactrTokenBadge/index.tsx
+++ src/components/V3/FactrTokenBadge/index.tsx
@@ -13,19 +13,20 @@ const FactrTokenBadge: React.FC<FactrTokenBadgeProps> = ({
     icon = <EthFactrIcon />,
     name = 'Ethereum',
     percentage = '50%',
-    //background = '#F8F9FC'
-    background = 'red'
+    background = '#F8F9FC'
 }) => {
     return (
-        <Box display="flex" flexDirection="column" alignItems="center" sx={{ backgroundColor: background }} >
-            {icon}
-            <Typography variant="body2" color="textPrimary" fontWeight={600}>
-                {name}
-            </Typography>
-            <Typography variant="body2" color="textPrimary" fontSize={'13px'}>
-                {percentage}
-            </Typography>
-        </Box>
+        <Box display="flex" width={'100%'} alignItems="center" justifyContent="center" padding={6} sx={{ backgroundColor: "white" }}>
+            <Box display="flex" width="120px" height="152px" flexDirection="column" alignItems="center" justifyContent="center" sx={{ backgroundColor: background, borderRadius: "8px" }} >
+                {icon}
+                <Typography variant="body2" color="textPrimary" fontWeight={600}>
+                    {name}
+                </Typography>
+                <Typography variant="body2" color="textPrimary" fontSize="13px">
+                    {percentage}
+                </Typography>
+            </Box>
+        </Box >
     );
 };
 
