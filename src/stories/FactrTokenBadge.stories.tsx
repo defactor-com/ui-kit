f892565f597df896acbf4bc11c4eba69981c89af
pzagardedukic <petra.zagar.dedukic@gmail.com>
2024-10-08 11:53:35 +0200

    feat(v3): FactrTokenBadge Fixes v2. #409

--- src/stories/FactrTokenBadge.stories.tsx
+++ src/stories/FactrTokenBadge.stories.tsx
@@ -1,6 +1,7 @@
 import React from "react";
 import { Story, Meta } from "@storybook/react";
 import FactrTokenBadge, { FactrTokenBadgeProps } from "../components/V3/FactrTokenBadge";
+import DocIcon from "../components/Icons/v2/docIcon";
 
 export default {
     title: "V3/FactrTokenBadge",
@@ -13,7 +14,7 @@ const Template: Story<FactrTokenBadgeProps> = (args) => (
 
 export const BasicFactrTokenBadge = Template.bind({});
 BasicFactrTokenBadge.args = {
-    icon: "/v3-ethFactrToken-icon.svg", // Path to the icon in the public folder
-    name: "Ethereum",                    // Example token name
-    percentage: "50%",                   // Example percentage
+    icon: <DocIcon />,
+    name: "Ethereum",
+    percentage: "50%",
 };
