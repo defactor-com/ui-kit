f5cc59d9982253af75b1bde4de14c008a010d0f0
pzagardedukic <petra.zagar.dedukic@gmail.com>
2024-10-09 14:15:47 +0200

    feat(v3): Added title and descrition as props. #409

--- src/components/V3/EmptyTable/index.tsx
+++ src/components/V3/EmptyTable/index.tsx
@@ -3,7 +3,15 @@ import { Box, Typography, useTheme } from '@mui/material'
 import CircleIcon from '../CircleIcon'
 import { CoinsStacked01 } from '@untitled-ui/icons-react';
 
-export const EmptyTable: React.FC = () => {
+interface EmptyTableProps {
+    title?: string;
+    description?: string;
+}
+
+export const EmptyTable: React.FC<EmptyTableProps> = ({
+    title = 'No assets to display',
+    description = 'You can create a new asset from existing community templates or by creating your own template first.'
+}) => {
     const theme = useTheme()
 
     return (
@@ -26,7 +34,7 @@ export const EmptyTable: React.FC = () => {
                 icon={<CoinsStacked01 style={{ width: 40, height: 40, color: '#5A5BEB' }} />}
             />
             <Typography variant='h6' fontWeight={700}>
-                No assets to display
+                {title}
             </Typography>
             <Box
                 sx={{
@@ -36,10 +44,10 @@ export const EmptyTable: React.FC = () => {
             >
                 <Typography variant='body1' sx={{ color: theme.palette.grey[400] }}>
 
-                    You can create a new asset from existing community templates or by creating your own template first.
+                    {description}
                 </Typography>
             </Box>
-        </Box>
+        </Box >
     )
 }
 
