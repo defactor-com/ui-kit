67e8c2eb7499fd3dd7f38488f093a10264189e80
pzagardedukic <petra.zagar.dedukic@gmail.com>
2024-10-22 10:04:03 +0200

    feat(MultiChainSelectorV3): Updated Main Component and Added Export. #414

--- src/components/MultichainSelectorV3/index.tsx
+++ src/components/MultichainSelectorV3/index.tsx
@@ -10,12 +10,9 @@ import {
 import CheckBoxIcon from "@mui/icons-material/CheckBox";
 import CheckBoxOutlineBlankIcon from "@mui/icons-material/CheckBoxOutlineBlank";
 
-import {
-  IMultiChainSelector,
-  NetworksDataType,
-} from "./MultiChainSelectorTypesV3"
+import { IMultiChainSelector, NetworksDataType } from "./MultiChainSelectorTypesV3";
 
-export const MultichainSelectorV3 = ({
+export const MultichainSelectorV3: React.FC<IMultiChainSelector> = ({
   networkSelected,
   networksAssets,
   textFieldLabel,
@@ -23,79 +20,76 @@ export const MultichainSelectorV3 = ({
   fontFamily,
   onClick,
   color,
-}: IMultiChainSelector) => (
-  <FormControl
-    sx={{
-      maxWidth: 240,
-      fontFamily: fontFamily,
-      width: (networksList.length === 1 ? 160 : 87) * networksList.length,
-    }}
-  >
-    <Select
-      multiple
-      sx={{
-        height: 40,
-        borderRadius: "30px",
-      }}
-      className="selector"
-      value={networkSelected}
-      id="multichain-selector"
-      renderValue={(selected) => (
-        <Box display="flex" flexWrap="wrap">
-          {selected.map((value) => (
-            <Box mr={1} display="flex" alignItems="center" key={value.chainId}>
-              <img
-                width={24}
-                height={24}
-                alt={`Networt ${value.name} icon`}
-                src={networksAssets[value.chainId]?.logo || ""}
-              />
-            </Box>
-          ))}
-          <Typography ml={1} variant="body1" fontWeight={700}>
-            {textFieldLabel}
-          </Typography>
+}) => {
+  const renderValue = (selected: NetworksDataType[]) => (
+    <Box display="flex" flexWrap="wrap">
+      {selected.map((value) => (
+        <Box mr={1} display="flex" alignItems="center" key={value.chainId}>
+          <img
+            width={24}
+            height={24}
+            alt={`Network ${value.name} icon`}
+            src={networksAssets[value.chainId]?.logo || ""}
+          />
         </Box>
-      )}
-      MenuProps={{
-        PaperProps: {
-          style: {
-            maxWidth: 240,
-            fontFamily: fontFamily,
-            maxHeight: 48 * 4.5 + 8,
-            width: (networksList.length === 1 ? 160 : 87) * networksList.length,
-          },
-        },
+      ))}
+      <Typography ml={1} variant="body1" fontWeight={700}>
+        {textFieldLabel}
+      </Typography>
+    </Box>
+  );
+
+  return (
+    <FormControl
+      sx={{
+        maxWidth: 240,
+        fontFamily,
+        width: (networksList.length === 1 ? 160 : 87) * networksList.length,
       }}
     >
-      {networksList.map((chain: NetworksDataType) => (
-        <MenuItem sx={{ padding: 0 }}>
-          <Box
-            display="flex"
-            alignItems="center"
-            onClick={() => onClick(chain)}
-          >
-            <Checkbox
-              icon={<CheckBoxOutlineBlankIcon />}
-              checkedIcon={<CheckBoxIcon sx={{ color: color }} />}
-              checked={
-                !!networkSelected.find((net) => net.chainId === chain.chainId)
-              }
-            />
-            <Box mr={1} display="flex" alignItems="center">
-              <img
-                width={24}
-                height={24}
-                alt={`Networt ${chain.name} icon`}
-                src={networksAssets[chain.chainId].logo}
+      <Select
+        multiple
+        value={networkSelected}
+        id="multichain-selector"
+        sx={{
+          height: 40,
+          borderRadius: "30px",
+        }}
+        renderValue={renderValue}
+        MenuProps={{
+          PaperProps: {
+            style: {
+              maxWidth: 240,
+              fontFamily,
+              maxHeight: 48 * 4.5 + 8,
+              width: (networksList.length === 1 ? 160 : 87) * networksList.length,
+            },
+          },
+        }}
+      >
+        {networksList.map((chain: NetworksDataType) => (
+          <MenuItem key={chain.chainId} sx={{ padding: 0 }}>
+            <Box display="flex" alignItems="center" onClick={() => onClick(chain)}>
+              <Checkbox
+                icon={<CheckBoxOutlineBlankIcon />}
+                checkedIcon={<CheckBoxIcon sx={{ color }} />}
+                checked={!!networkSelected.find((net) => net.chainId === chain.chainId)}
               />
+              <Box mr={1} display="flex" alignItems="center">
+                <img
+                  width={24}
+                  height={24}
+                  alt={`Network ${chain.name} icon`}
+                  src={networksAssets[chain.chainId]?.logo || ""}
+                />
+              </Box>
+              <Typography variant="body1" fontFamily={fontFamily}>
+                {chain.name}
+              </Typography>
             </Box>
-            <Typography variant="body1" fontFamily={fontFamily}>
-              {chain.name}
-            </Typography>
-          </Box>
-        </MenuItem>
-      ))}
-    </Select>
-  </FormControl>
-);
+          </MenuItem>
+        ))}
+      </Select>
+    </FormControl>
+  );
+};
