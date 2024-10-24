bcfd1feb6469c11198941533c7521faa070f53fd
pzagardedukic <petra.zagar.dedukic@gmail.com>
2024-10-24 09:18:36 +0200

    feat(V3): CircleIconV3: Added some props. #418

--- src/components/V3/CircleIconV3/index.tsx
+++ src/components/V3/CircleIconV3/index.tsx
@@ -3,21 +3,27 @@ import { CoinsStacked01 } from '@untitled-ui/icons-react';
 import { Box } from '@mui/material';
 
 interface CircleIconProps {
+    width?: string,
+    height?: string,
+    borderRadius?: '50%',
     backgroundColor?: string;
     icon?: React.ReactElement;
 }
 
 const CircleIconV3: React.FC<CircleIconProps> = ({
+    width = 88,
+    height = 88,
+    borderRadius = '50%',
     backgroundColor = '#EFEFFD',
     icon = <CoinsStacked01 style={{ width: 40, height: 40, color: '#5A5BEB' }} />,
 }) => {
     return (
         <Box
             sx={{
-                width: 88,
-                height: 88,
-                borderRadius: '50%',
-                backgroundColor,
+                width: width,
+                height: height,
+                borderRadius: borderRadius,
+                backgroundColor: backgroundColor,
                 display: 'flex',
                 justifyContent: 'center',
                 alignItems: 'center',
