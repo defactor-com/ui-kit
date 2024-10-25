32227b4fa66261939076d3b7fcad2183ef238363
pzagardedukic <petra.zagar.dedukic@gmail.com>
2024-10-25 14:10:06 +0200

    feat(V3): Added main files - PercentageCircle. #434

---
+++ src/stories/PercentageCircle.stories.tsx
@@ -0,0 +1,19 @@
+import React from 'react';
+import { Story, Meta } from '@storybook/react';
+import { PercentageCircle, PercentageCircleProps } from '../components/V3/PercentageCircle';
+import { EthFactrIcon } from '../components/V3/PercentageCircle/EthFactrIcon';
+
+export default {
+    title: 'Components/V3/PercentageCircle',
+    component: PercentageCircle,
+} as Meta;
+
+const Template: Story<PercentageCircleProps> = (args) => <PercentageCircle {...args} />;
+
+export const Default = Template.bind({});
+Default.args = {
+    percentage: '75',
+    icon: <EthFactrIcon width={49} height={49} />,
+};
+
+
