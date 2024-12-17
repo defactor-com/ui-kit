75379bcd458d0266cac212eadcaed2616a52122e
pzagardedukic <petra.zagar.dedukic@gmail.com>
2024-12-17 11:38:01 +0100

    feat(MainSidebar): Added demo engage routes. #464

---
+++ src/components/MainSidebar/icons/types.ts
@@ -0,0 +1,10 @@
+export type IconsType = {
+    color?: string
+    width?: number
+    stroke?: string
+    height?: number
+    colorEllipseOne?: string
+    colorEllipseTwo?: string
+    colorEllipseFour?: string
+    colorEllipseThree?: string
+}
