ab90db2bf6924e34923b05a1776de763a3ee4449
pzagardedukic <petra.zagar.dedukic@gmail.com>
2024-12-23 14:26:12 +0100

    feat(MobileMenuV3): AppbarV3: Added demo props. #476

--- src/components/V3/AppbarV3/index.tsx
+++ src/components/V3/AppbarV3/index.tsx
@@ -1,98 +1,103 @@
 import {
-  AppBar,
-  Box,
-  IconButton,
-  Toolbar,
-  useMediaQuery,
-  useTheme,
-} from "@mui/material";
-import { Menu01, XClose } from "@untitled-ui/icons-react";
-import Link from "next/link";
-import React, { useEffect, useState } from "react";
-
-//import WalletSelector from 'app/components/WalletV2'
-
-//import ClaimTokensButton from '../ClaimTokensButton'
-import { LanguageSelectorV3 } from "../LanguageSelectorV3";
-import { MobileMenuV3 } from "../MobileMenuV3";
-import ClaimTokensButtonV3 from "../ClaimTokensButtonV3";
-
-export const generalConfig = {
-  routesDisabled: [],
-};
-
-const AppbarV3: React.FC = () => {
-  const theme = useTheme();
-  const isMobile = useMediaQuery(theme.breakpoints.down("sm"));
-  const [open, setOpen] = useState<boolean>(false);
-
-  useEffect(() => {
-    if (!isMobile && open) {
-      setOpen(false);
-    }
-  }, [isMobile, open]);
-
-  return (
-    <AppBar
-      component={"header"}
-      position="fixed"
-      sx={{
-        zIndex: (theme) => theme.zIndex.drawer + 1,
-        height: 60,
-        backgroundColor: "white",
-        boxShadow: "8px 10px 10px 0px #D6DAE740",
-        pr: "0px !important",
-      }}
-    >
-      <Toolbar>
-        <Box sx={{ flexGrow: 1 }}>
-          <Link href="/">
-            {/* eslint-disable-next-line @next/next/no-img-element */}
-            <img
-              src={generalConfig.assets.appLogo}
-              alt={"headerLogo"}
-              style={{ marginTop: 6 }}
-            />
-          </Link>
-        </Box>
-        {!isMobile ? (
-          <>
-            {generalConfig.appEnvironment === "testnet" && (
-              <ClaimTokensButtonV3 />
-            )}
-            {/*  <WalletSelector /> */}
-            <LanguageSelectorV3
-              router={() => {}}
-              pathname="/"
-              locale="en"
-              t={(key: string, { locale }: { locale: string }) => `${locale}`}
-            />
-          </>
-        ) : (
-          <>
-            <Box height="22px" margin={2}>
-              <Link href="/" passHref>
-                {/* eslint-disable-next-line @next/next/no-img-element */}
-                <img src={generalConfig.assets.assets.src} alt="Assets" />
-              </Link>
-            </Box>
-            <IconButton
-              sx={{
-                p: 0,
-                width: "40px",
-                height: "40px",
-                borderRadius: "50%",
-              }}
-              onClick={() => setOpen(!open)}
-            >
-              {!open ? <Menu01 /> : <XClose />}
-            </IconButton>
-          </>
-        )}
-      </Toolbar>
-      <MobileMenuV3 open={open} onClose={() => setOpen(false)} />
-    </AppBar>
-  );
-};
-
-export default AppbarV3;
+    AppBar,
+    Box,
+    IconButton,
+    Toolbar,
+    useMediaQuery,
+    useTheme,
+  } from "@mui/material";
+  import { Menu01, XClose } from "@untitled-ui/icons-react";
+  import Link from "next/link";
+  import React, { useEffect, useState } from "react";
+  
+  import { LanguageSelectorV3 } from "../LanguageSelectorV3";
+  import { MobileMenuV3 } from "../MobileMenuV3";
+  import ClaimTokensButtonV3 from "../ClaimTokensButtonV3";
+  
+  export const generalConfig = {
+    routesDisabled: [],
+  };
+  
+  interface AppbarV3Props {
+    appLogo?: string;
+    appLogoAlt?: string;
+    appEnvironment?: string;
+    currentLogo?: string;
+    currentLogoAlt?: string;
+  }
+  
+  const AppbarV3: React.FC<AppbarV3Props> = ({
+    appLogo = "/default-logo.png",
+    appLogoAlt = "Header Logo",
+    appEnvironment = "production",
+    currentLogo = "/default-mobile-logo.png",
+    currentLogoAlt = "Default Mobile Logo",
+  }) => {
+    const theme = useTheme();
+    const isMobile = useMediaQuery(theme.breakpoints.down("sm"));
+    const [open, setOpen] = useState<boolean>(false);
+  
+    useEffect(() => {
+      if (!isMobile && open) {
+        setOpen(false);
+      }
+    }, [isMobile, open]);
+  
+    return (
+      <AppBar
+        component={"header"}
+        position="fixed"
+        sx={{
+          zIndex: (theme) => theme.zIndex.drawer + 1,
+          height: 60,
+          backgroundColor: "white",
+          boxShadow: "8px 10px 10px 0px #D6DAE740",
+          pr: "0px !important",
+        }}
+      >
+        <Toolbar>
+          <Box sx={{ flexGrow: 1 }}>
+            <Link href="/">
+              {/* eslint-disable-next-line @next/next/no-img-element */}
+              <img src={appLogo} alt={appLogoAlt} style={{ marginTop: 6 }} />
+            </Link>
+          </Box>
+          {!isMobile ? (
+            <>
+              {appEnvironment === "testnet" && <ClaimTokensButtonV3 />}
+              <LanguageSelectorV3
+                router={() => {}}
+                pathname="/"
+                locale="en"
+                t={(key: string, { locale }: { locale: string }) => `${locale}`}
+              />
+            </>
+          ) : (
+            <>
+              <Box height="22px" margin={2}>
+                <Link href="/" passHref>
+                  {/* eslint-disable-next-line @next/next/no-img-element */}
+                  <img src={currentLogo} alt={currentLogoAlt} />
+                </Link>
+              </Box>
+              <IconButton
+                sx={{
+                  p: 0,
+                  width: "40px",
+                  height: "40px",
+                  borderRadius: "50%",
+                }}
+                onClick={() => setOpen(!open)}
+              >
+                {!open ? <Menu01 /> : <XClose />}
+              </IconButton>
+            </>
+          )}
+        </Toolbar>
+        <MobileMenuV3 open={open} onClose={() => setOpen(false)} />
+      </AppBar>
+    );
+  };
+  
+  export default AppbarV3;
+  
