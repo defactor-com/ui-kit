cb8f31d55b511b11642faf858b13d6fcd921fdd1
AngeloCG97 <angelocas13_8@hotmail.com>
2025-01-10 15:31:15 -0600

    feat(linechart): added condition to hide checkboks section

--- src/components/LineChart/ChartsTypes.ts
+++ src/components/LineChart/ChartsTypes.ts
@@ -45,6 +45,7 @@ export interface ILineChart extends IChart {
   series: SeriesDataType[];
   filterBgColor?: string;
   currentFilter?: string;
+  showCheckBox?: boolean;
   dateFilter?: string[];
   color?: string;
 }
