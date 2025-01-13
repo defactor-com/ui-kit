c516a8b413df1a89fcacd3127cad83b6be228793
pzagardedukic <petra.zagar.dedukic@gmail.com>
2025-01-13 11:03:29 +0100

    feat(MobileMenuV3): Refactor MobileMenuV3... #515

--- src/components/V3/MobileMenuV3/MobileMenuTypes.ts
+++ src/components/V3/MobileMenuV3/MobileMenuTypes.ts
@@ -1,13 +1,28 @@
-import { AppDataType } from "../../Sidebar/SidebarTypes";
+import { Route, AppData } from "../../MainSidebar/icons/types";
 
 export interface IMenuMobileV3 {
   fontFamily: string;
   marginLeft?: number | string;
   marginRight?: number | string;
-  mainApp: AppDataType;
   languageLabel: string;
-  appsData?: Array<AppDataType>;
   menuOptions?: React.ReactNode;
   languageSelector: React.ReactNode;
+  mainApp?: AppData;
+  appsData?: AppData[];
+  mainSidebarBgColor?: string;
+  navLinkTextColor?: string;
+  iconsColor?: string;
+  activeTextColor?: string;
+  activeIconColor?: string;
+  activeBorderColor?: string;
+  notificationColor?: string;
+  notificationsCount?: number;
+  hideOnBreakpoint?: "xs" | "sm" | "md" | "lg" | "xl";
+  defaultPath?: string;
+  routes?: Route[];
+  mt?: number | string;
+  selectedBgColor?: string;
+  roles?: Record<string, string>;
+  userContext?: { role: string };
   onClick?: (path: string) => void;
 }
--- src/components/V3/MobileMenuV3/index.tsx
+++ src/components/V3/MobileMenuV3/index.tsx
@@ -9,6 +9,7 @@ import {
   Typography,
   Divider,
 } from "@mui/material";
+import { useTheme } from "@mui/material/styles";
 
 import menuIcon from "/assets/menu-icon.svg";
 import linkIcon from "/assets/link-icon.svg";
@@ -17,210 +18,88 @@ import closeMenuIcon from "/assets/close-mobile-icon.svg";
 import { IMenuMobileV3 } from "./MobileMenuTypes";
 import MainMenuItem from "../../MainSidebar/MenuItem";
 import { Route } from "../../MainSidebar/icons/types"; 
-import { routes } from "../../MainSidebar/demoRoutes";
-import { useTheme } from "@mui/material/styles"; 
-
-const style = {
-  top: "50%",
-  left: "50%",
-  width: "100%",
-  height: "100%",
-  bgcolor: "background.paper",
-  transform: "translate(-50%, -50%)",
-  position: "absolute" as "absolute",
-};
 
 export const MobileMenuV3 = ({
-  mainApp,
-  appsData,
   fontFamily,
   languageLabel,
   languageSelector,
-  marginRight = 0,
+  mainApp,
+  appsData,
   marginLeft = 1,
-  onClick
+  marginRight = 0,
+  navLinkTextColor = useTheme().palette.text.primary,
+  iconsColor = useTheme().palette.text.secondary,
+  activeTextColor = useTheme().palette.text.primary,
+  activeIconColor = useTheme().palette.primary.main,
+  activeBorderColor = useTheme().palette.primary.dark,
+  notificationColor = useTheme().palette.error.main,
+  selectedBgColor = useTheme().palette.action.selected,
+  notificationsCount = 5,
+  roles = { admin: 'admin' },
+  userContext = { role: 'admin' },
+  onClick,
+  routes = [] // provide a default routes array or from a context/store if applicable
 }: IMenuMobileV3) => {
   const [open, setOpen] = useState(false);
   const handleClose = () => setOpen(false);
   const handleOpen = () => setOpen(true);
-  const theme = useTheme();
-
-  const context = { role: 'admin' }; // Example context, adjust based on your actual context logic
-  const roles = { admin: 'admin' }; // Example roles, adjust as needed
-  const notificationsCount = 5; // Example count, adjust as needed
-  const navLinkTextColor = theme.palette.text.primary;
-  const iconsColor = theme.palette.text.secondary;
-  const activeTextColor = theme.palette.text.primary;
-  const activeIconColor = theme.palette.primary.main;
-  const activeBorderColor = theme.palette.primary.dark;
-  const notificationColor = theme.palette.error.main;
-  const selectedBgColor = theme.palette.action.selected;
 
   return (
     <Box mr={marginRight} ml={marginLeft}>
-      <IconButton
-        onClick={handleOpen}
-        sx={{ borderRadius: "20px", border: "1.25px solid #EDF0F7" }}
-      >
+      <IconButton onClick={handleOpen} sx={{ borderRadius: "20px", border: "1.25px solid #EDF0F7" }}>
         <Image src={menuIcon} width={16} height={17} alt="Menu selector icon" />
       </IconButton>
-      <Modal
-        open={open}
-        onClose={handleClose}
-        aria-labelledby="modal-modal-title"
-        aria-describedby="modal-modal-description"
-      >
-        <Box sx={style}>
-          <Box
-            paddingX={2}
-            paddingY={3}
-            display="flex"
-            alignItems="center"
-            justifyContent="space-between"
-          >
-            <Image
-              alt="Tool icon"
-              src={mainApp.logo.src}
-              width={mainApp.logo.width}
-              height={mainApp.logo.height}
-            />
-            <IconButton
-              sx={{
-                width: "40px",
-                height: "40px",
-                borderRadius: "20px",
-                border: "1.25px solid #EDF0F7",
-              }}
-              onClick={handleClose}
-            >
-              <Image
-                width={16}
-                height={16}
-                src={closeMenuIcon}
-                alt="Close menu icon"
-              />
+      <Modal open={open} onClose={handleClose} aria-labelledby="modal-modal-title" aria-describedby="modal-modal-description">
+        <Box sx={{
+          top: "50%",
+          left: "50%",
+          width: "100%",
+          height: "100%",
+          bgcolor: "background.paper",
+          transform: "translate(-50%, -50%)",
+          position: "absolute"
+        }}>
+          <Box paddingX={2} paddingY={3} display="flex" alignItems="center" justifyContent="space-between">
+            <Image src={mainApp?.logo.src} alt="Tool icon" width={mainApp?.logo.width} height={mainApp?.logo.height} />
+            <IconButton onClick={handleClose} sx={{ width: "40px", height: "40px", borderRadius: "20px", border: "1.25px solid #EDF0F7" }}>
+              <Image src={closeMenuIcon} width={16} height={16} alt="Close menu icon" />
             </IconButton>
           </Box>
           <Box pb={2} height="100%" sx={{ overflowY: "auto" }}>
             <Box pb={2} onClick={handleClose}>
               <Box sx={{ overflow: "auto" }}>
-                {[...routes().firstRoutes]
-                  .filter(
-                    (route: Route) => route.public || context.role === roles.admin
-                  )
-                  .map((route: Route, index: number) => (
-                    <MainMenuItem
-                      key={index}
-                      icon={route.icon}
-                      text={route.text}
-                      path={route.path}
-                      isSelected={route.path === "/" ? true : false} // Modify as needed
-                      notificationsCount={
-                        route.path === "/notifications" ? notificationsCount : 0
-                      }
-                      navLinkTextColor={navLinkTextColor}
-                      iconsColor={iconsColor}
-                      activeTextColor={activeTextColor}
-                      activeIconColor={activeIconColor}
-                      activeBorderColor={activeBorderColor}
-                      notificationColor={notificationColor}
-                      selectedBgColor={selectedBgColor}
-                      onClick={() => {
-                        if (onClick) onClick(route.path);
-                      }}
-                    />
-                  ))}
-                {routes().secondRoutes.length > 0 && (
-                  <Divider
-                    sx={{
-                      border: `${theme.palette.grey[300]} 1px solid`,
-                    }}
+                {routes.filter(route => route.public || userContext.role === roles.admin).map((route, index) => (
+                  <MainMenuItem
+                    key={index}
+                    icon={route.icon}
+                    text={route.text}
+                    path={route.path}
+                    isSelected={route.path === "/" ? true : false}
+                    notificationsCount={route.path === "/notifications" ? notificationsCount : 0}
+                    navLinkTextColor={navLinkTextColor}
+                    iconsColor={iconsColor}
+                    activeTextColor={activeTextColor}
+                    activeIconColor={activeIconColor}
+                    activeBorderColor={activeBorderColor}
+                    notificationColor={notificationColor}
+                    selectedBgColor={selectedBgColor}
+                    onClick={() => onClick?.(route.path)}
                   />
-                )}
-                {[...routes().secondRoutes]
-                  .filter(
-                    (route: Route) => route.public || context.role === roles.admin
-                  )
-                  .map((route: Route, index: number) => (
-                    <MainMenuItem
-                      key={index}
-                      icon={route.icon}
-                      text={route.text}
-                      path={route.path}
-                      isSelected={route.path === "/" ? true : false}
-                      notificationsCount={
-                        route.path === "/notifications" ? notificationsCount : 0
-                      }
-                      navLinkTextColor={navLinkTextColor}
-                      iconsColor={iconsColor}
-                      activeTextColor={activeTextColor}
-                      activeIconColor={activeIconColor}
-                      activeBorderColor={activeBorderColor}
-                      notificationColor={notificationColor}
-                      selectedBgColor={selectedBgColor}
-                      onClick={() => {
-                        if (onClick) onClick(route.path);
-                      }}
-                    />
-                  ))}
+                ))}
               </Box>
             </Box>
-            <Box
-              p={2}
-              display="flex"
-              alignItems="center"
-              borderTop="1px solid #EDF0F7"
-              justifyContent="space-between"
-            >
-              <Typography variant="body1" sx={{ fontFamily: fontFamily }}>
-                {languageLabel}
-              </Typography>
+            <Box p={2} display="flex" alignItems="center" borderTop="1px solid #EDF0F7" justifyContent="space-between">
+              <Typography variant="body1" sx={{ fontFamily }}>{languageLabel}</Typography>
               {languageSelector}
             </Box>
           </Box>
           {appsData && (
-            <Box
-              bottom={0}
-              width="100%"
-              height="53px"
-              display="flex"
-              position="absolute"
-              borderTop="1px solid #EDF0F7"
-            >
-              {appsData.map((item) => (
-                <Button
-                  fullWidth
-                  sx={{
-                    display: "flex",
-                    alignItems: "center",
-                    justifyContent: "center",
-                    borderRight: "1px solid #EDF0F7",
-                  }}
-                >
-                  <Link
-                    href={item.url}
-                    target="_blank"
-                    sx={{
-                      display: "flex",
-                      alignItems: "center",
-                      justifyContent: "center",
-                    }}
-                  >
-                    <Image
-                      width={65}
-                      height={21}
-                      alt="Tool logo"
-                      src={item.logo.src}
-                      style={{
-                        marginRight: "8px",
-                      }}
-                    />
-                    <Image
-                      width={12}
-                      height={12}
-                      src={linkIcon}
-                      alt="Link icon"
-                    />
+            <Box bottom={0} width="100%" height="53px" display="flex" position="absolute" borderTop="1px solid #EDF0F7">
+              {appsData.map((item, index) => (
+                <Button key={index} fullWidth sx={{ display: "flex", alignItems: "center", justifyContent: "center", borderRight: "1px solid #EDF0F7" }}>
+                  <Link href={item.url} target="_blank" sx={{ display: "flex", alignItems: "center", justifyContent: "center" }}>
+                    <Image src={item.logo.src} width={65} height={21} alt="Tool logo" style={{ marginRight: "8px" }} />
+                    <Image src={linkIcon} width={12} height={12} alt="Link icon" />
                   </Link>
                 </Button>
               ))}
