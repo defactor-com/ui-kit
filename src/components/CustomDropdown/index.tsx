d58a5e7980dba65a47b16ad114483367c0c902c1
pzagardedukic <petra.zagar.dedukic@gmail.com>
2025-02-28 12:02:42 +0100

    feat(assets): Revert CustomDropzone. #560

--- src/components/CustomDropdown/index.tsx
+++ src/components/CustomDropdown/index.tsx
@@ -9,7 +9,6 @@ import {
   OutlinedInput,
 } from "@mui/material";
 import React from "react";
-import { ChevronDown } from "@untitled-ui/icons-react";
 
 import { CustomTooltip } from "../CustomTooltip";
 
@@ -34,147 +33,78 @@ export interface CustomDropdownProps {
   required?: boolean;
   tooltipBgColor: string;
   focusedColor?: string;
-  paddingSize?: string;
-  flexDirectionRow?: boolean;
-  numColor?: string;
-  numSize?: string;
 }
 
 export const CustomDropdown: React.FC<CustomDropdownProps> = ({
   label = "",
   placeholder = "Choose...",
   value = "",
-  onChange = () => {},
+  onChange = () => { },
   menuItems = AssetTypes,
   tooltip = "",
   disabled = false,
   required = false,
   tooltipBgColor,
-  paddingSize,
-  focusedColor,
-  flexDirectionRow = false,
-  numColor,
-  numSize,
+  focusedColor
 }) => {
   const theme = useTheme();
 
   const handleChange = (event: SelectChangeEvent<string>) => {
-    if (onChange) {
-      onChange(event.target.value);
-    }
+      if (onChange) {
+          onChange(event.target.value);
+      }
   };
 
   return (
-    <FormControl
-      variant="outlined"
-      fullWidth
-      disabled={disabled}
-      sx={{
-        flexDirection: flexDirectionRow ? "row" : "column",
-      }}
-    >
-      <Box
-        sx={{
-          display: "flex",
-          flexDirection: "row",
-          alignItems: "center",
-          height: "28px",
-        }}
-      >
-        {label && (
-          <>
-            <Typography variant="caption" sx={{ opacity: 0.5 }}>
-              {label}
-            </Typography>
-            {required && (
-              <Typography
-                variant="caption"
-                color={theme.palette.warning.main}
-                fontWeight={700}
-                pl={0.4}
-              >
-                *
-              </Typography>
-            )}
-          </>
-        )}
-        {tooltip && (
-          <CustomTooltip
-            tooltipText={tooltip}
-            tooltipBgColor={tooltipBgColor}
-          />
-        )}
-      </Box>
-
-      <Select
-        value={value || ""}
-        displayEmpty
-        IconComponent={(props) => (
+      <FormControl variant="outlined" fullWidth disabled={disabled}>
           <Box
-            component="span"
-            sx={{
-              display: "flex",
-              alignItems: "center",
-              justifyContent: "center",
-              height: "100%",
-            }}
-          >
-            <ChevronDown
-              {...props}
-              style={{
-                width: "14px",
+              sx={{
+                  display: "flex",
+                  flexDirection: "row",
+                  alignItems: "center",
+                  minHeight: "34px",
               }}
-            />
-          </Box>
-        )}
-        sx={{
-          color: numColor,
-          fontSize: numSize,
-          ".MuiSelect-select": {
-            display: "flex",
-            alignItems: "center",
-            paddingTop: `${paddingSize}`,
-            paddingBottom: `${paddingSize}`,
-          },
-          "& .MuiOutlinedInput-root": {
-            display: "flex",
-            alignItems: "center",
-            height: "100%",
-          },
-          "& .MuiSelect-icon": {
-            top: "50%",
-            transform: "translateY(-50%)",
-          },
-        }}
-        renderValue={(selected) =>
-          selected || (
-            <Typography sx={{ opacity: 0.5 }}>{placeholder}</Typography>
-          )
-        }
-        onChange={handleChange}
-        input={
-          <OutlinedInput
-            sx={{
-              height: "28px",
-              "&.Mui-focused .MuiOutlinedInput-notchedOutline": {
-                borderColor: focusedColor || theme.palette.secondary.main,
-              },
-            }}
-          />
-        }
-      >
-        {menuItems.map((menuItem) => (
-          <MenuItem
-            key={menuItem.label}
-            value={menuItem.value}
-            sx={{
-              fontSize: numSize || "12px",
-            }}
           >
-            {menuItem.label}
-          </MenuItem>
-        ))}
-      </Select>
-    </FormControl>
+              <Typography variant="caption" sx={{ opacity: 0.5 }}>
+                  {label}
+              </Typography>
+              {required && (
+                  <Typography
+                      variant="caption"
+                      color={theme.palette.warning.main}
+                      fontWeight={700}
+                      pl={0.4}
+                  >
+                      *
+                  </Typography>
+              )}
+              {tooltip && <CustomTooltip tooltipText={tooltip} tooltipBgColor={tooltipBgColor} />}
+          </Box>
+          <Select
+              value={value || ""}
+              displayEmpty
+              renderValue={(selected) =>
+                  selected || (
+                      <Typography sx={{ opacity: 0.5 }}>{placeholder}</Typography>
+                  )
+              }
+              onChange={handleChange}
+              input={
+                  <OutlinedInput
+                    sx={{
+                      "&.Mui-focused .MuiOutlinedInput-notchedOutline": {
+                        borderColor: focusedColor || theme.palette.secondary.main,
+                      },
+                    }}
+                  />
+                }
+          >
+              {menuItems.map((menuItem) => (
+                  <MenuItem key={menuItem.label} value={menuItem.value}>
+                      {menuItem.label}
+                  </MenuItem>
+              ))}
+          </Select>
+      </FormControl>
   );
 };
