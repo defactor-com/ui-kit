e18ed4a750feead474104dc2fb216bf5f265eb22
pzagardedukic <petra.zagar.dedukic@gmail.com>
2025-03-28 09:59:28 +0100

    feat(ui-kit): AreaChartV3: Updated Storybook File: more data, reduced chart width. #581

--- src/stories/AreaChartV3.stories.tsx
+++ src/stories/AreaChartV3.stories.tsx
@@ -1,6 +1,7 @@
 import React from "react";
 import { Story } from "@storybook/react";
 import { AreaChartV3, AreaChartV3Props } from "../components/V3/AreaChartV3";
+import { Box } from "@mui/material";
 
 export default {
   title: "V3/AreaChartV3",
@@ -15,7 +16,11 @@ export default {
   },
 };
 
-const Template: Story<AreaChartV3Props> = (args) => <AreaChartV3 {...args} />;
+const Template: Story<AreaChartV3Props> = (args) => (
+  <Box sx={{ width: "374px" }}>
+    <AreaChartV3 {...args} />
+  </Box>
+);
 
 export const Default = Template.bind({});
 Default.args = {
@@ -27,5 +32,17 @@ Default.args = {
     { time: "14:00", value: 310000 },
     { time: "16:00", value: 350000 },
     { time: "00:00", value: 390000 },
-  ]
+    { time: "00:00", value: 140000 },
+    { time: "04:00", value: 210000 },
+    { time: "08:00", value: 250000 },
+    { time: "12:00", value: 300000 },
+    { time: "14:00", value: 310000 },
+    { time: "16:00", value: 350000 },
+    { time: "00:00", value: 390000 },
+    { time: "08:00", value: 250000 },
+    { time: "12:00", value: 300000 },
+    { time: "14:00", value: 310000 },
+    { time: "16:00", value: 350000 },
+    { time: "00:00", value: 390000 },
+  ],
 };
