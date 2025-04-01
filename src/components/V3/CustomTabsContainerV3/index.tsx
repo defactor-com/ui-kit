c1448701496e33a9da7b447efc61cd881b769205
pzagardedukic <petra.zagar.dedukic@gmail.com>
2025-04-01 10:24:56 +0200

    feat(ui-kit): Added CustomTabsContainerV3 component. #587

---
+++ src/components/V3/CustomTabsContainerV3/index.tsx
@@ -0,0 +1,62 @@
+import { TabContext, TabList, TabPanel } from '@mui/lab'
+import { Box, Tab } from '@mui/material'
+import React from 'react'
+
+export interface TabItem {
+  value: string
+  label: string
+  icon?: React.ReactElement | string
+  tabsContent: React.ReactNode
+}
+
+export interface CustomTabContainerProps {
+  value: string
+  onChange: (event: React.SyntheticEvent, newValue: string) => void
+  items: TabItem[]
+}
+
+const CustomTabsContainerV3: React.FC<CustomTabContainerProps> = ({
+  value,
+  onChange,
+  items
+}) => {
+  return (
+    <Box>
+      <TabContext value={value}>
+        <Box sx={{ borderBottom: 1, borderColor: 'divider' }}>
+          <TabList
+            onChange={onChange}
+            aria-label='custom tabs'
+            textColor='inherit'
+          >
+            {items.map(tab => (
+              <Tab
+                key={tab.value}
+                value={tab.value}
+                label={tab.label}
+                icon={tab.icon || undefined}
+                iconPosition='start'
+                sx={{
+                  padding: 1,
+                  minWidth: 30,
+                  minHeight: 0,
+                  fontWeight: 700,
+                  fontSize: '18px',
+                  pr: { xs: 3, md: 6 },
+                  pl: 3
+                }}
+              />
+            ))}
+          </TabList>
+        </Box>
+        {items.map(tab => (
+          <TabPanel key={tab.value} value={tab.value}>
+            {tab.tabsContent}
+          </TabPanel>
+        ))}
+      </TabContext>
+    </Box>
+  )
+}
+
+export default CustomTabsContainerV3
